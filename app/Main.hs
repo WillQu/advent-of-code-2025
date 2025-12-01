1921d29f629a29616116608a1864c632c9177cd1
Sébastien Willmann <sebastien.willmann@hcl-software.com>
2025-12-01 17:41:25 +0100

    Day 1 Part 2

--- app/Main.hs
+++ app/Main.hs
@@ -6,12 +6,22 @@ main = do
   day1 "day1.example"
   putStrLn "Day 1"
   day1 "day1"
+  putStrLn "Day 1 part 2 example"
+  day1part2 "day1.example"
+  putStrLn "Day 1 part 2"
+  day1part2 "day1"
 
 day1 :: String -> IO ()
 day1 file = do
   input <- readInput file
   let parsed = mapM readLine input
   either putStrLn (print . countZeros) parsed
+
+day1part2 :: String -> IO ()
+day1part2 file = do
+  input <- readInput file
+  let parsed = mapM readLine input
+  either putStrLn (print . countZeros2) parsed
 
 readInput :: String -> IO [String]
 readInput file = fmap lines $ readFile file
@@ -24,5 +34,17 @@ readLine other = Left $ "Can’t read " ++ other
 countZeros :: [Int] -> Int
 countZeros list = length $ filter (== 0) $ scanl rotate 50 list
 
+countZeros2 list = zeros $ foldl rotate2 (Rotations 50 0) list
+
 rotate :: Int -> Int -> Int
 rotate x y = (x + y) `mod` 100
+
+data Rotations = Rotations { current :: Int , zeros :: Int }
+
+crossZero :: Int -> Int -> Int
+crossZero x 0 = 0
+crossZero x y | y > 0 = (x + y) `div` 100
+crossZero x y | y < 0 = (((100 - x) `mod` 100)  - y) `div` 100
+
+rotate2 :: Rotations -> Int -> Rotations
+rotate2 (Rotations current zeros) x = Rotations (rotate current x) (zeros + (crossZero current x))
